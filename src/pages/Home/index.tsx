8d0979fa8a2d8cfcc324865f44f73628ac481f27
Agustin Gomez <agusleongomez@gmail.com>
2022-12-14 11:54:42 -0300

    created data grid

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,49 @@
 import React from 'react';
+import { DataGrid, GridRenderCellParams } from '@mui/x-data-grid';
+import { People } from '@/data';
+
+console.log(0.1 + 0.2 === 0.3);
 
 export interface HomeInterface {}
 
 const Home: React.FC<HomeInterface> = () => {
-  return <div>Home</div>;
+  const pageSize = 5;
+  const columns = [
+    {
+      field: 'name',
+      headerName: 'Name',
+      flex: 1,
+      minWidth: 150,
+      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+    },
+    {
+      field: 'category',
+      headerName: 'Category',
+      flex: 1,
+      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+    },
+    {
+      field: 'company',
+      headerName: 'Company',
+      flex: 1,
+      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+    },
+  ];
+
+  return (
+    <div>
+      <DataGrid
+        rows={People}
+        columns={columns}
+        disableColumnSelector
+        disableSelectionOnClick
+        autoHeight
+        pageSize={pageSize}
+        rowsPerPageOptions={[pageSize]}
+        getRowId={(row: any) => row.id}
+      />
+    </div>
+  );
 };
 
 export default Home;
