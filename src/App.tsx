415f3d272e77a88e4a7c281d1902ff65f8e46225
Agustin Gomez <agusleongomez@gmail.com>
2022-12-17 17:28:41 -0300

    added styledLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { Navbar } from './components';
 import { Home } from './pages';
+import { LayoutContainer } from './styled-components';
 
 function App() {
   return (
     <>
       <Navbar />
-      <Home />
+      <LayoutContainer>
+        <Home />
+      </LayoutContainer>
     </>
   );
 }
---
+++ src/styled-components/layout.styled.component.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const LayoutContainer = styled.div`
+  margin: 15vh auto;
+
+  @media only screen and (min-width: 1241px) {
+    width: 80vw;
+  }
+
+  @media only screen and (max-width: 1176px) {
+    padding: 12px;
+  }
+`;
